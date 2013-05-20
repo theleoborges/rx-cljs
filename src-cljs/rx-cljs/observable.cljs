e4fcee6c95cbcb07fcd8d2b900f9653ad05b1cca
Leonardo Borges <leonardoborges.rj@gmail.com>
2013-05-20 13:33:51 +1000

    Backed off from the Protocol based impl in favour of pure functions. The overhead added by protocols isn't necessary at this stage

--- src-cljs/rx-cljs/observable.cljs
+++ src-cljs/rx-cljs/observable.cljs
@@ -1,30 +1,20 @@
 (ns rx-cljs.observable
   (:refer-clojure :exclude [map reduce]))
 
-(defprotocol IObservable
-  (subscribe [_ f])
-  (map       [_ f])
-  (reduce    [_ f])
-  (select-many [_ sel]))
-
-(defrecord Observable [observable]
-  IObservable
-  (subscribe [_ f]
-    (.subscribe observable f))
-  
-  (map [_ f]
-    (Observable. (.map observable f)))
-  
-  (reduce [_ f]
-    (Observable. (.reduce observable f)))
-  
-  (select-many [_ sel]
-    (Observable. (.selectMany observable
-                              (fn [& args]
-                                (:observable (apply sel args)))))))
-
 (defn return-value [v]
-  (Observable. (.returnValue js/Rx.Observable v)))
+  (.returnValue js/Rx.Observable v))
 
 (defn from-array [coll]
-  (Observable. (.fromArray js/Rx.Observable (clj->js coll))))
+  (.fromArray js/Rx.Observable (clj->js coll)))
+
+(defn subscribe [observable f]
+  (.subscribe observable f))
+
+(defn map [observable f]
+  (.map observable f))
+
+(defn reduce [observable f]
+  (.reduce observable f))
+
+(defn select-many [observable sel]
+  (.selectMany observable sel))
